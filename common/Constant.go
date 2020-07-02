e9f91bbb3c7d572f205497278abff697549bfbd7
Aleistar Markóczy <a.markoczy@gmail.com>
2020-07-02 22:53:19 +0200

    Constant validate

--- common/Constant.go
+++ common/Constant.go
@@ -1,16 +1,28 @@
 package common
 
+import "fmt"
+
 type Constant struct {
 	identifier string
 	value      float64
+}
+
+func NewConstant(identifier string, value float64) *Constant {
+	return &Constant{
+		identifier: identifier,
+		value:      value,
+	}
 }
 
 func (c *Constant) Parametrized() bool {
 	return false
 }
 
-func (c *Constant) Validate(params ...Node) bool {
-	return len(params) == 0
+func (c *Constant) Validate(params ...Node) error {
+	if len(params) != 0 {
+		return fmt.Errorf("Expected no arguments but got: %v", params)
+	}
+	return nil
 }
 
 func (c *Constant) AsNode(params ...Node) Node {
